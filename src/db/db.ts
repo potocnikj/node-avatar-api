030bef71586c351a544945e23f84e854a047eff6
Jan Potočnik <potocnik.fmf@gmail.com>
2018-07-21 17:03:50 +0200

    added

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,19 +1,11 @@
 import * as mysql from 'mysql';
-import * as config from 'config';
-
 import { environment } from '../environment/environment';
 
 export class Db {
-
-    //TODO: When creating a connection to the DB host, a random read host must be selected and pinged if it's alive.
-    //TODO: In case of DB server unavailability, other hosts must be tried until :
-    //TODO: A --> A living server has been found, in this case, connect to it.
-    //TODO: B --> All servers had been found, return a JSON response with 500 status and explanation of DB failure.
     /**
      * Statically instantiates a new MySQL connection.
      */
     public static initMySQL(){
-        console.log(environment);
         const connection  = mysql.createConnection({
             host            : environment.db.host[0],
             user            : environment.db.user,
@@ -31,10 +23,9 @@ export class Db {
 
         return connection;
     }
-
 }
 
 /**
  * Exports a long-lived connection as a singleton.
  */
-export var connection = Db.initMySQL();
+// export var connection = Db.initMySQL();
